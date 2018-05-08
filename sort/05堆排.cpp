1a13fd5f414c0c2142355cd5ac4c3771e8b920d2
asdfghwunai <1729589198@qq.com>
2018-05-08 17:04:41 +0800

    Create 05堆排.cpp

---
+++ sort/05堆排.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+void Heap_adjust(int a[],int first,int last)
+{
+	int temp = a[first];
+	for (int j = 2 * first; j <= last; j++)
+	{
+		if (j<last&&a[j + 1]>a[j])   //j<last是为了有j+1
+			j++;      //找到孩子中最大的
+		if (temp > a[j])   //构建成大顶推
+			break;
+		swap(a[first],a[j]);
+		first = j;    //迁移到孩子
+	}
+}
+
+void  Heap_sort(int a[],int first,int last)
+{
+	for (int i = (last - first + 1) / 2 - 1; i >= 0; i--)  //自下向上,自右向左调整
+		Heap_adjust(a,i,last);
+
+	for (int i = last; i > 0; i--)
+	{
+		swap(a[0], a[i]);  //与最后一个交换
+		Heap_adjust(a, 0, i-1);  //其余的调整一下
+	}
+}
+
+void main()
+{
+	int a[] = {4,3,7,2,9,0,-1};
+	Heap_sort(a,0,6);
+
+	for (int i = 0;i<sizeof(a)/sizeof(int);i++)
+	{
+		cout <<a[i] <<" " ;
+	}
+	
+
+	while (1);
+}
